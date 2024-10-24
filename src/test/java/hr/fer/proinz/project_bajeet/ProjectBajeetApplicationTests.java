548dc15fa85cb91057558b890cc1c4feab081dab
root <root@localhost.localdomain>
2024-10-24 18:26:23 +0000

    Project template

---
+++ src/test/java/hr/fer/proinz/project_bajeet/ProjectBajeetApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.proinz.project_bajeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBajeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
