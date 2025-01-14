960ce2923272f9adc0e73456adfa137d4822048c
1212mauro <1212maurom@gmail.com>
2025-01-14 23:23:14 +0100

    Update Registracija.jsx
    resolve conflicts

--- IzvorniKod/frontend/src/pages/Registracija.jsx
+++ IzvorniKod/frontend/src/pages/Registracija.jsx
@@ -1,6 +1,8 @@
 // Registracija.js
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Input from '../components/Input';
+import RegisterFooter from '../components/RegisterFooter';
 
 const Registracija = () => {
   const [username, setUsername] = useState('');
@@ -51,44 +53,12 @@ const Registracija = () => {
     <div className="max-w-sm mx-auto mt-16 p-8 border rounded-3xl shadow-lg">
       <h2 className="text-2xl text-center mb-4">Registracija</h2>
       {error && <div className="text-red-500 text-center mb-4">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Korisničko ime</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full p-2 mt-2 border rounded-md"
-            placeholder="Unesite korisničko ime"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Lozinka</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 mt-2 border rounded-md"
-            placeholder="Unesite lozinku"
-            required
-          />
-        </div>
-    
-        <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md">
-          Registriraj se
-        </button>
-      </form>
-      <div className="text-center mt-4">
-        <p>Već imate račun?</p>
-        <Link to="/" className="text-blue-500 hover:underline">
-          Povratak na prijavu
-        </Link>
-      </div>
+      <Input label="username" type="text" labelClassName="block text-sm font-medium text-gray-700" inputClassName="w-full p-2 mt-2 border rounded-md" stateSetter={setUsername}/>
+      <Input label="password" type="password" labelClassName="block text-sm font-medium text-gray-700" inputClassName="w-full p-2 mt-2 border rounded-md" stateSetter={setPassword}/>
+      <button onClick={handleSubmit} className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-700">
+        Registriraj se
+      </button>
+      <RegisterFooter />
     </div>
   );
 };
