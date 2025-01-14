b5889f9ef77661a53bcac9d17e0a1fb9eec18a0b
1212mauro <1212maurom@gmail.com>
2025-01-14 23:43:18 +0100

    fix: deploy error

--- IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/data/VoteRepository.java
+++ IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/data/VoteRepository.java
@@ -1,9 +1,11 @@
 package hr.fer.proinz.project_bajeet.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import hr.fer.proinz.project_bajeet.dataTypes.Vote;
 
+@Repository
 public interface VoteRepository extends JpaRepository<Vote, Integer>{
     Vote findByVoteID(Integer voteId);
 }
