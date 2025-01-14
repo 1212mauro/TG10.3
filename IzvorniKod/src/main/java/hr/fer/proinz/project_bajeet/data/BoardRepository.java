e07a350e6614833675e9e0067049f25a82abfd76
1212mauro <1212maurom@gmail.com>
2025-01-14 23:50:02 +0100

    fix: deploy error (#47)

--- IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/data/BoardRepository.java
+++ IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/data/BoardRepository.java
@@ -1,11 +1,12 @@
 package hr.fer.proinz.project_bajeet.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import hr.fer.proinz.project_bajeet.dataTypes.Board;
 import java.util.List;
 
-
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Integer>{
     Board findByBoardID(Integer BoardId);
     List<Board> findByAddress(String address);
