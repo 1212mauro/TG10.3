ab39a484a72021af53101c429f23c2ab5be5a97a
Marko Vukadin <mv54623@fer.hr>
2025-01-15 12:59:47 +0100

    Security

--- IzvorniKod/frontend/src/components/LoginFooter.jsx
+++ IzvorniKod/frontend/src/components/LoginFooter.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 
 
 const redirectToOAuth = (url) => {
-    localStorage.setItem('authToken', url); 
     window.location.href = url;
 };
 
--- IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/config/SecurityConfig.java
+++ IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(
-                    "/**", "/**").permitAll()
+                    "/auth/**", "/oauth2/**").permitAll()
                     .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
