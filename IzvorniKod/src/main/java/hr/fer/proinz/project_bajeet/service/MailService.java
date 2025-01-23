62ff13e3e93b59923b689d9dce88bd6fa31d3e4f
1212mauro <1212maurom@gmail.com>
2025-01-23 20:46:11 +0100

    feat:notifications via email (#79)

---
+++ IzvorniKod/src/main/java/hr/fer/proinz/project_bajeet/service/MailService.java
@@ -0,0 +1,29 @@
+package hr.fer.proinz.project_bajeet.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    
+    //@Value("$(spring.mail.username)")
+    private String fromEmailId = "project.bajeet@gmail.com";
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String recipient, String subject, String body){
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(fromEmailId);
+        simpleMailMessage.setTo(recipient);
+        simpleMailMessage.setText(body);
+        simpleMailMessage.setSubject(subject);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
